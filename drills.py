905ecda066a18f7e3037a0c20bd286f2b9e4764b
jderouchey <joel.derouchey@gmail.com>
2023-10-03 17:54:25 -0700

    Update drills.py
    updated syntax

--- drills.py
+++ drills.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """drill.py: Simple math drills."""
 
@@ -6,43 +6,36 @@ __author__ = "Joel DeRouchey"
 __copyright__ = "No Copyright 2017, Reno, Nevada"
 
 import random
-import datetime, time
+import datetime
 
 count = 1
 correct = 0
-questions = list(xrange(145))
-equations = list(xrange(145))
+questions = [0] * 145
+equations = [None] * 145
 
-for x1 in xrange(1, 13 ):
-    for x2 in xrange(x1, 13):
+for x1 in range(1, 13):
+    for x2 in range(x1, 13):
         answer = x1 * x2
         questions[count] = answer
-        equations[count] = str(x1) + ' * ' + str(x2) + ' = '
-#        print ('%i.  %i * %i = %i' % (count, x1, x2, questions[count]))
+        equations[count] = f'{x1} * {x2} = '
         count += 1
 
+rq = random.sample(range(1, 79), 78)
 
-
-rq = random.sample(xrange(1, 79), 78)
-
-# rq = random questions
 then = datetime.datetime.now()
 for q in rq:
-    estimation = input(equations[q] + ' ')
+    estimation = int(input(equations[q]))
     if estimation == questions[q]:
-        print 'Correct'
+        print('Correct')
         correct += 1
     else:
-        print 'Incorrect'
+        print('Incorrect')
 now = datetime.datetime.now()
 duration = now - then
-print 'You have answered ' + str(correct) + ' out of 78' + ' in ' + str(duration)
-print 'Accuracy :' + str(correct/float(78)) 
-print 'Speed is :' + str(duration.seconds/float(78))
+print(f'You have answered {correct} out of 78 in {duration}')
+print(f'Accuracy: {correct / 78:.2%}')
+print(f'Speed: {duration.total_seconds() / 78:.2f} seconds per question')
 
-f = open('math.log', 'a')
-ts = time.time()
-st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d,%H:%M:%S')
-f.write(st)
-f.write(',' + str(correct/float(78))) 
-f.write(',' + str(duration.seconds/float(78)))
+with open('math.log', 'a') as f:
+    st = datetime.datetime.now().strftime('%Y-%m-%d,%H:%M:%S')
+    f.write(f'{st},{correct / 78},{duration.total_seconds() / 78}\n')
